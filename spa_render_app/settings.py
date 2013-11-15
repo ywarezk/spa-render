743a834cb29fb21ba94192a6dffac80a7ad89320
Yariv Katz <yariv@nerdeez.com>
2013-11-15 17:37:36 +0200

    fixed the logging

--- spa_render_app/settings.py
+++ spa_render_app/settings.py
@@ -84,3 +84,32 @@ INSTALLED_APPS = INSTALLED_APPS + ('spa_render_app',)
 import os.path
 #TEMPLATE_DIRS = TEMPLATE_DIRS + (os.path.join(os.path.dirname(__file__), 'templates').replace('\\','/'),)
 INSTALLED_APPS = INSTALLED_APPS + ('gunicorn',)
+
+# A sample logging configuration. The only tangible logging
+# performed by this configuration is to send an email to
+# the site admins on every HTTP 500 error when DEBUG=False.
+# See http://docs.djangoproject.com/en/dev/topics/logging for
+# more details on how to customize your logging configuration.
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'filters': {
+        'require_debug_false': {
+            '()': 'django.utils.log.RequireDebugFalse'
+        }
+    },
+    'handlers': {
+        'mail_admins': {
+            'level': 'ERROR',
+            'filters': ['require_debug_false'],
+            'class': 'django.utils.log.AdminEmailHandler'
+        }
+    },
+    'loggers': {
+        'django.request': {
+            'handlers': ['mail_admins'],
+            'level': 'ERROR',
+            'propagate': True,
+        },
+    }
+}
