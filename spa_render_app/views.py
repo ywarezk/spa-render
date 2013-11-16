75fbbc4714ff4c7862b66fe479073b8dec3c875b
Yariv Katz <yariv@nerdeez.com>
2013-11-16 13:31:20 +0200

    opened it to all urls

--- spa_render_app/views.py
+++ spa_render_app/views.py
@@ -2,9 +2,14 @@ from django.http import HttpResponse
 import subprocess
 
 def home(request):
+    '''
+    will render a page with its javascript
+    @param url: passed via get param containing the url to render
+    '''
+    url = request.GET.get('url')
     text = subprocess.check_output([
                 'phantom/phantomjs-linux', 
                 'phantom/phantom-server.js', 
-                'http://www.nerdeez.com'
+                url
             ])
     return HttpResponse(text)
